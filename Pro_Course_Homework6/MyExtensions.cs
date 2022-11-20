649d155606ae20c67d034fc3facf819b5ad05525
Olexander <73075612+alexkrnnk@users.noreply.github.com>
2022-11-20 12:52:29 +0200

    Create class MyExtensions.cs
    Сlass MyExtensions where extension method implementation.

---
+++ Pro_Course_Homework6/MyExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    internal static class MyExtensions
+    {
+        public static int Round(this double value)
+        {
+            return (int)Math.Round(value, MidpointRounding.AwayFromZero);
+        }
+    }
+}
